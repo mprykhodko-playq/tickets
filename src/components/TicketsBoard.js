462e9efb20effb0b88b89641e7f262ede35008db
mikeprikhodko <mprykhodko@playq.net>
2021-03-06 16:03:23 +0200

    add filters

--- src/components/TicketsBoard.js
+++ src/components/TicketsBoard.js
@@ -12,7 +12,11 @@ class TicketsBoard extends Component {
     constructor() {
         super();
         this.state = {
-            all: false
+            all: true,
+            none: false,
+            one: false,
+            two: false,
+            three: false
         };
         this.onChange = this.onChange.bind(this);
     }
@@ -35,16 +39,28 @@ class TicketsBoard extends Component {
         let items = [];
 
         const TicketAlgorithm = tickets => {
-            if (tickets.length < 1) {
+
+            for (let i = 0; i < tickets.length; i++){
+                if (this.state.all){
+                    items.push(<TicketItem key={i} ticket={tickets[i]}/>)
+                } else {
+                    const none = this.state.none && ((tickets[i].segments[0].stops.length == 0) || (tickets[i].segments[1].stops.length == 0));
+                    const one = this.state.one && ((tickets[i].segments[0].stops.length == 1) || (tickets[i].segments[1].stops.length == 1));
+                    const two = this.state.two && ((tickets[i].segments[0].stops.length == 2) || (tickets[i].segments[0].stops.length == 2));
+                    const three = this.state.three && ((tickets[i].segments[0].stops.length == 3) || (tickets[i].segments[0].stops.length == 3));
+
+                    if (none || one || two || three){
+                        items.push(<TicketItem key={i} ticket={tickets[i]}/>)
+                    }
+                }
+            }
+
+            if (items.length < 1){
                 return(
                     <div className="alert alert-info text-center" role="alert">
                         No tickets
                     </div>
                 )
-            } else {
-                for (let i = 0; i < tickets.length; i++){
-                    items.push(<TicketItem key={i} ticket={tickets[i]}/>)
-                }
             }
 
             return (
@@ -75,19 +91,66 @@ class TicketsBoard extends Component {
                 <br/>
                 <hr/>
 
-                <form>
-                    <div className="card-group">
-                        <Checkbox
-                            className="text-left card-body"
-                            checked={this.state.all}
-                            name = "all"
-                            disableRipple={true}
-                            tabIndex={-1}
-                            onChange={this.onChange}
-                        />
-                        <ListItemText primary="All" className="text-left card-body"/>
+                <div className="card mb-1 bg-light col-md-4">
+                    <div className="card-body bg-light">
+                        <h5>Количество пересадок</h5>
+                        <div className="card-group justify-content-center">
+                            <Checkbox
+                                className="text-left card-body"
+                                checked={this.state.all}
+                                name = "all"
+                                disableRipple={true}
+                                tabIndex={-1}
+                                onChange={this.onChange}
+                            />
+                            <ListItemText primary="Все" className="text-left card-body"/>
+                        </div>
+                        <div className="card-group justify-content-center">
+                            <Checkbox
+                                className="text-left card-body"
+                                checked={this.state.none}
+                                name = "none"
+                                disableRipple={true}
+                                tabIndex={-1}
+                                onChange={this.onChange}
+                            />
+                            <ListItemText primary="Без пересадок" className="text-left card-body"/>
+                        </div>
+                        <div className="card-group">
+                            <Checkbox
+                                className="text-left card-body"
+                                checked={this.state.one}
+                                name = "one"
+                                disableRipple={true}
+                                tabIndex={-1}
+                                onChange={this.onChange}
+                            />
+                            <ListItemText primary="1 пересадка" className="text-left card-body"/>
+                        </div>
+                        <div className="card-group">
+                            <Checkbox
+                                className="text-left card-body"
+                                checked={this.state.two}
+                                name = "two"
+                                disableRipple={true}
+                                tabIndex={-1}
+                                onChange={this.onChange}
+                            />
+                            <ListItemText primary="2 пересадки" className="text-left card-body"/>
+                        </div>
+                        <div className="card-group">
+                            <Checkbox
+                                className="text-left card-body"
+                                checked={this.state.three}
+                                name = "three"
+                                disableRipple={true}
+                                tabIndex={-1}
+                                onChange={this.onChange}
+                            />
+                            <ListItemText primary="3 пересадки" className="text-left card-body"/>
+                        </div>
                     </div>
-                </form>
+                </div>
 
                 {TicketContent}
             </div>
